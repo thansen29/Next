36a50bc7db64350e870c16c5f8ed6c1b417d9759
Thomas Hansen <thansen1694@gmail.com>
2018-05-31 22:37:34 -0400

    Fatal debugger left in

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,30 +1,29 @@
 class Api::TasksController < ApplicationController
-    def index 
+    def index
         @tasks = Task.all
             .where(list_id: params[:id])
-    end 
+    end
 
     def all
         @tasks = Task.all
             .where(user_id: current_user.id)
-        
+
             render :index
-    end 
+    end
 
     def create
         @task = Task.new(task_params)
         @task.user_id = current_user.id
-        debugger
         if @task.save
             render :show
         else
           render json: { route: "You must give your task a title" }, status: 422
-        end 
-    end 
+        end
+    end
 
     def show
         @task = Task.find(params[:id])
-    end 
+    end
 
     # separate when editing title, description, or completion
     def update
@@ -32,30 +31,30 @@ class Api::TasksController < ApplicationController
         if params["title"] || params["description"]
             if params["title"]
                 @task.title = params["title"]
-            end 
+            end
 
             if params["description"]
                 @task.description = params["description"]
-            end 
-        else 
+            end
+        else
             @task.completed = !@task.completed
-        end 
+        end
 
         if @task.save!
             render :show
-        else 
+        else
             render json: { error: @task.errors.full_messages }
-        end 
-    end 
+        end
+    end
 
     def destroy
         task = Task.find(params[:id])
         task.destroy!
         render json: { message: "Deleted the record"}
-    end 
+    end
 
 
     def task_params
         params.require(:task).permit(:title, :description, :list_id)
-    end 
+    end
 end
